51205d68973b5097d20ff7f26d53051eaadbdb98
Guido Pacienzia <guidoalpino4@gmail.com>
2025-10-26 20:07:39 -0300

    cambio teest variables

--- TestTransporte/TestTransporte/UnitTest1.cs
+++ TestTransporte/TestTransporte/UnitTest1.cs
@@ -5,18 +5,20 @@ namespace TestTransporte
 {
     public class Tests
     {
+        private TransporteUrbano.Colectivo colectivo132;
+        private TransporteUrbano.Colectivo colectivo145_133;
         [SetUp]
         public void Setup()
         {
-            TransporteUrbano.Colectivo colectivo145_133 = new TransporteUrbano.Colectivo("145 133");
-            TransporteUrbano.Colectivo colectivo132 = new TransporteUrbano.Colectivo("132");
+            colectivo145_133 = new TransporteUrbano.Colectivo("145/133");
+            colectivo132 = new TransporteUrbano.Colectivo("132");
             
         }
 
         [Test]
         public void Test1()
         {
-            
         }
     }
+
 }
